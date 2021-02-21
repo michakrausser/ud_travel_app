6b3a7bbca0c71e30b77c791be966fa30ba86bcbd
Micha Kraußer <michak@synphonyte.com>
2021-02-21 12:32:36 -0600

    Webpack set up etc.

---
+++ src/server/index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(`Your API key is ${ process.env.API_KEY }`);
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve('dist/index.html'))
+})
+
+app.get('/api-key', function (req, res) {
+  res.send({ apiKey: process.env.API_KEY })
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(3333, function () {
+  console.log('Example app listening on port 3333!')
+})
